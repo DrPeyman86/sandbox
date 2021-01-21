d0e4d74708af409b954df5470dc48facb4377e9d
Peyman-PC <peymanc@hotmail.com>
2021-01-21 12:32:24 -0500

    fixed bug

--- Node_Js/fileReadDemo.js
+++ Node_Js/fileReadDemo.js
@@ -1,6 +1,15 @@
 var fs = require('fs')
 
-fs.readdir('c:/', (err,data) => {
-    console.log(data);
-})
+// fs.readdir('c:/', (err,data) => {
+//     console.log(data);
+// })
 
+fs.mkdirSync('./uploads/packingLists', {recursive: true}, (err,data)=>{
+    if(err) {
+        //if path exists it will enter here
+        console.log(err)
+    } else {
+        //if path does not exist, it will enter here
+        console.log(data)
+    }
+})
